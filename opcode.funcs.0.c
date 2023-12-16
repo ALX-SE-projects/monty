09361b4e347a8617d03f046f898556a2752fc3d3
ibrahemesam <iimosa7777@gmail.com>
2023-12-16 21:16:40 +0200

    blabla bla

--- opcode.funcs.0.c
+++ opcode.funcs.0.c
@@ -12,7 +12,7 @@ void __push(stack_t **h, int val)
 void __pint(__attribute__((unused)) stack_t **h, char *strLineNumber)
 {
 	if (*h)
-		printf("%d\n", (*doubly)->n);
+		printf("%d\n", (*h)->n);
 	else
 		exit_with_err(
 			concat(3, "L", strLineNumber, ": can't pint, stack empty\n"));
